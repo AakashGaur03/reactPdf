386b57f031e4be301955680bcc3cbd994d9ad2f0
AakashGaur03 <aakash.sunrise23@gmail.com>
2024-02-18 22:30:54 +0530

    Exploring

--- src/Components/DownloadablePDF.jsx
+++ src/Components/DownloadablePDF.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import { Document, Page, Text, View, StyleSheet, BlobProvider } from '@react-pdf/renderer';
+import React from "react";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  BlobProvider,
+} from "@react-pdf/renderer";
 
 const DownloadablePDF = ({ content }) => {
   const handleDownloadPDF = ({ blob, url }) => {
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
-    link.download = 'document.pdf';
+    link.download = "document.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-
 
     //PDF is downloading without both these
     // document.body.appendChild(link);
     // document.body.removeChild(link);
     //But however it's good practice to include the append and remove steps to ensure proper cleanup
-
   };
 
   return (
     <BlobProvider document={<PDFDocument content={content} />}>
       {({ blob, url, loading, error }) => (
         <div>
-          <button onClick={() => handleDownloadPDF({ blob, url })}>Download PDF</button>
+          <button onClick={() => handleDownloadPDF({ blob, url })}>
+            Download PDF
+          </button>
         </div>
       )}
     </BlobProvider>
@@ -43,6 +51,17 @@ const PDFDocument = ({ content }) => (
             <Text style={styles.cell}>{item.name}</Text>
             <Text style={styles.cell}>{item.id}</Text>
             <Text style={styles.cell}>{item.Title}</Text>
+            {/* <Image src='https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg' style={styles.image}>
+            </Image> */}
+            {/* <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgpqUTC0FB8XolnZ-75g0KM9Ss2iTz53evVTbi0C8HLx6vUW7R6BzsFWA2mGFe-h98ALY&usqp=CAU' style={styles.image}>
+            </Image> */}
+
+            <Image src='checkbox-5.png' style={{height:"20px" , width:"20px"}}>
+            </Image>
+            <Image src='checkboxCircle.png' style={{height:"20px" , width:"20px"}}>
+            </Image>
+            <Image src='Circle.png' style={{height:"20px" , width:"20px"}}>
+            </Image>
           </View>
         ))}
       </View>
@@ -52,8 +71,8 @@ const PDFDocument = ({ content }) => (
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'column', // Set direction to column
-    backgroundColor: 'purple', // Set background color to purple
+    flexDirection: "column", // Set direction to column
+    backgroundColor: "purple", // Set background color to purple
   },
   section: {
     margin: 10,
@@ -61,19 +80,25 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   tableRow: {
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderColor: '#000000',
+    flexDirection: "row",
+    // borderBottomWidth: 1,
+    borderColor: "#000000",
     marginBottom: 5,
   },
   headerCell: {
     flex: 1,
     fontSize: 12,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   cell: {
     flex: 1,
     fontSize: 10,
+  },
+  image: {
+    flex: 1,
+    fontSize: 10,
+    height:20,
+    width:20,
   },
 });
 
--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
-import DownloadablePDF from './DownloadPDF';
+import DownloadablePDF from './DownloadablePDF';
 function MainComponent() {
   const content = ['Item 1', 'Item 2', 'Item 3'];
   const obj= [{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   },{
     name:"Aakash",
     id:"",
-    Title:"Downloading"
+    Title:"Downloading",
+    imageSrc:"../../public/vite.svg"
   }]
 
   return (
     <div>
       <h1>Downloadable PDF</h1>
+      <img src="../../public/checkbox-5.png" alt="" height={20} width={20} />
       <DownloadablePDF content={obj} />
     </div>
   );
